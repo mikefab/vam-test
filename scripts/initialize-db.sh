0d305c9d22ec07b90c9be2c19eae2658e72f78ba
mikefab <mikefabrikant@gmail.com>
2019-06-16 17:19:07 -0400

    Remove from root

--- scripts/initialize-db.sh
+++
@@ -1,8 +0,0 @@
-DB=${MONGODB_DBNAME:-mydb}
-	# initializing '${DBENGINE}' database '${DBNAME}'
-	if [ $(mongo localhost:27017 --eval "db.getMongo().getDBNames().indexOf('mydb')" --quiet) -lt 0 ]; then
-	    echo "mydb does not exist. Creating now..."
-      mongo localhost:27017/${DB} --eval 'db.createCollection("forms", {} )'
-	else
-	    echo "mydb exists"
-	fi
