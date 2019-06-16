1c7e650e0d61c834c51d24779b8d3e4e00bd36fd
mikefab <mikefabrikant@gmail.com>
2019-06-16 17:18:49 -0400

    Unnecessary single quotes

--- app/scripts/initialize-db.sh
+++ app/scripts/initialize-db.sh
@@ -1,8 +1,8 @@
 DB=${MONGODB_DBNAME:-mydb}
 	# initializing '${DBENGINE}' database '${DBNAME}'
-	if [ $(mongo 'db:27017'  --eval "db.getMongo().getDBNames().indexOf('mydb')" --quiet) -lt 0 ]; then
+	if [ $(mongo db:27017  --eval "db.getMongo().getDBNames().indexOf('mydb')" --quiet) -lt 0 ]; then
 	    echo "mydb does not exist. Creating now..."
-      mongo 'db:27017' /${DB} --eval 'db.createCollection("forms", {} )'
+      mongo db:27017 /${DB} --eval 'db.createCollection("forms", {} )'
 	else
 	    echo "mydb exists"
 	fi
